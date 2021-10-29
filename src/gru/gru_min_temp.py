97c5b2f7a3b71079343453e4ce9116b25db037cb
Indira Dutta <66861242+indiradutta@users.noreply.github.com>
2021-10-29 22:58:26 +0530

    minor fixes

--- src/gru/gru_min_temp.py
+++ src/gru/gru_min_temp.py
@@ -14,15 +14,15 @@ origin = datetime.datetime(2017,4,1)
 for i in range(len(list(df['Date']))):
   df['Date'][i] = (datetime.datetime.strptime(df['Date'][i], '%d.%m.%Y') - origin).days
 
-x2 = df[["Date", "maxtemp", "pressure", "humidity", "mean wind speed", "weather"]].values
+x2 = df[["Date", "pressure", "humidity", "mean wind speed", "weather"]].values
 y2 = df["mintemp"].values
 
 scaler = preprocessing.StandardScaler()
 x2 = scaler.fit_transform(x2)
-x2 = x2.reshape(-1, 1, 6)
+x2 = x2.reshape(-1, 1, 5)
 
 regressor = Sequential()
-regressor.add(GRU(6, input_shape=(1,6), return_sequences=True))
+regressor.add(GRU(6, input_shape=(1,5), return_sequences=True))
 regressor.add(Dense(4, activation="relu"))
 regressor.add(Dense(1, activation="linear"))
 regressor.compile(optimizer='adam',loss='mean_squared_error')
