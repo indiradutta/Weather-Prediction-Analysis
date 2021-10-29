155ec3cb7064f031e4d90b24e7eda0ed4485695b
Indira Dutta <66861242+indiradutta@users.noreply.github.com>
2021-10-29 22:57:09 +0530

    minor fixes

--- src/ann/ann_min_temp.py
+++ src/ann/ann_min_temp.py
@@ -15,14 +15,14 @@ origin = datetime.datetime(2017,4,1)
 for i in range(len(list(df['Date']))):
   df['Date'][i] = (datetime.datetime.strptime(df['Date'][i], '%d.%m.%Y') - origin).days
 
-x2 = df[["Date", "maxtemp", "pressure", "humidity", "mean wind speed", "weather"]].values
+x2 = df[["Date", "pressure", "humidity", "mean wind speed", "weather"]].values
 y2 = df["mintemp"].values
 
 scaler = preprocessing.StandardScaler()
 x2 = scaler.fit_transform(x2)
 
 model = Sequential()
-model.add(Dense(8, input_dim=6, kernel_initializer='normal', activation='relu'))
+model.add(Dense(8, input_dim=5, kernel_initializer='normal', activation='relu'))
 model.add(Dense(4, activation="relu"))
 model.add(Dense(1, activation="linear"))
 model.compile(loss='mean_squared_error', optimizer='adam')
